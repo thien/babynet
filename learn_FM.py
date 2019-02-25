1eb689a94c1a6ef89b3c88378b23e543f276a02a
@thien <thien@users.noreply.github.com>
2019-02-25 22:37:58 +0000

    added changes to part 2 and part 3 to reflect changes to helper.py

--- learn_FM.py
+++ learn_FM.py
@@ -1,11 +1,12 @@
 import numpy as np
 import torch
-import torch.utils.data as utils
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
 from torch import optim
+import helpers
 
+from illustrate import illustrate_results_FM
 
 def main():
     dataset = np.loadtxt("FM_dataset.dat")
@@ -16,42 +17,19 @@ def main():
     
     trainRatio = 0.8
     testRatio  = 0.1
-    numEpochs  = 20
+    numEpochs  = 10
     batchsize  = 32
 
-    dataX, dataY = dataset[:, 0:3], dataset[:, 3:6]
-    # split the dataset
-    dataX = torch.stack([torch.Tensor(i) for i in dataX])
-    dataY = torch.stack([torch.Tensor(i) for i in dataY])
-    # normalise the data
-    dataX = F.normalize(dataX)
-    dataY = F.normalize(dataY)
-    # setup size ratios
-    dataSize       = int(dataset.shape[0])
-    trainingSize   = int(np.floor(dataSize * trainRatio))
-    leftoverSize   = int(dataSize - trainingSize)
-    testSize       = int(np.floor(leftoverSize * (testRatio/(1-trainRatio))))
-    validationSize = leftoverSize - testSize
+    # shuffle the dataset prior
+    np.random.shuffle(dataset)
+    # retrieve X and Y columns
+    X, Y = dataset[:, 0:3], dataset[:, 3:6]
 
-    trainX = dataX[:trainingSize]
-    valX   = dataX[trainingSize:trainingSize + validationSize]
-    testX  = dataX[trainingSize + validationSize:]
-    trainY = dataY[:trainingSize]
-    valY   = dataY[trainingSize:trainingSize + validationSize]
-    testY  = dataY[trainingSize + validationSize:]
+    # create loaders
+    trainloader, validloader, testloader = helpers.loadTrainingData(
+        X, Y, trainRatio, testRatio, batchsize, normalise=True)
 
-    trainloader = utils.DataLoader(
-        utils.TensorDataset(trainX, trainY), batch_size=batchsize)
-    validloader = utils.DataLoader(
-        utils.TensorDataset(valX, valY), batch_size=batchsize)
-    testloader = utils.DataLoader(
-        utils.TensorDataset(testX, testY), batch_size=batchsize)
-
-    # check for gpu.
-    device = "cpu"
-    print(device)
     # create the model.
-
     class Net(nn.Module):
         def __init__(self, in_size, out_size):
             super(Net, self).__init__()
@@ -68,62 +46,28 @@ def main():
             x = F.relu(self.a(x))
             x = F.relu(self.d(x))
             x = F.relu(self.e(x))
+            # we do not have an activation
+            # function on the last layer deliberately.
             x = self.f(x)
             return x
 
-    model = Net(3,3).to(device)
+    # initiate the model
+    model = Net(3,3)
 
+    # load hyperparameters
     loss_function = nn.MSELoss()
     optimizer     = optim.Adam(model.parameters())
 
-    for epoch in range(1, numEpochs):
-        
-        train_loss, valid_loss = [], []
-        # training part
-        model.train()
-        for data, target in trainloader:
-            data, target = data.to(device), target.to(device)
-            optimizer.zero_grad()
-            output = model(data)
-            loss = loss_function(output, target)
-            loss.backward()
-            optimizer.step()
-            train_loss.append(loss.item())
+    # train the model
+    helpers.trainModel(model, optimizer, loss_function, numEpochs, trainloader, validloader)
+    # evaluate the model
+    helpers.testModel(model, loss_function, numEpochs, testloader)
 
-        # evaluation part
-        model.eval()
-
-        for data, target in validloader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            loss = loss_function(output, target)
-            valid_loss.append(loss.item())
-
-        print("Epoch:", epoch, "\tTraining Loss: ", round(np.mean(
-            train_loss),4), "\tValid Loss: ", round(np.mean(valid_loss),4))
-
-    # test greatness of model
-    model.eval()
-    test_loss = 0
-    with torch.no_grad():
-        for data, target in testloader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            # sum up batch loss
-            # for i in range(len(output)):
-            #     print(output[i],target[i])
-            test_loss += loss_function(output, target).item()
-
-    test_loss /= len(testloader.dataset)
-
-    print('\nTest set: Average loss: {:.4f}'.format(
-        test_loss))
-
-        
     #######################################################################
     #                       ** END OF YOUR CODE **
     #######################################################################
-    # illustrate_results_FM(network, prep)
+    # data is normalised in this function
+    illustrate_results_FM(model)
 
 
 if __name__ == "__main__":
--- learn_ROI.py
+++ learn_ROI.py
@@ -5,17 +5,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
 from torch import optim
+import helpers
 
-from nn_lib import (
-    MultiLayerNetwork,
-    Trainer,
-    Preprocessor,
-    save_network,
-    load_network,
-)
 
-# from illustrate import illustrate_results_ROI
-
+from illustrate import illustrate_results_ROI
 
 def main():
     dataset = np.loadtxt("ROI_dataset.dat")
@@ -29,36 +22,12 @@ def main():
     numEpochs = 20
     batchsize = 64
 
-    dataX, dataY = dataset[:, 0:3], dataset[:, 3:]
-    # split the dataset
-    dataX = torch.stack([torch.Tensor(i) for i in dataX])
-    dataY = torch.stack([torch.Tensor(i) for i in dataY])
-    # setup size ratios
-    dataSize = int(dataset.shape[0])
-    trainingSize = int(np.floor(dataSize * trainRatio))
-    leftoverSize = int(dataSize - trainingSize)
-    testSize = int(np.floor(leftoverSize * (testRatio/(1-trainRatio))))
-    validationSize = leftoverSize - testSize
+    X, Y = dataset[:, 0:3], dataset[:, 3:]
 
-    trainX = dataX[:trainingSize]
-    valX = dataX[trainingSize:trainingSize + validationSize]
-    testX = dataX[trainingSize + validationSize:]
-    trainY = dataY[:trainingSize]
-    valY = dataY[trainingSize:trainingSize + validationSize]
-    testY = dataY[trainingSize + validationSize:]
+    trainloader, validloader, testloader = helpers.loadTrainingData(
+        X, Y, trainRatio, testRatio, batchsize)
 
-    trainloader = utils.DataLoader(
-        utils.TensorDataset(trainX, trainY), batch_size=batchsize)
-    validloader = utils.DataLoader(
-        utils.TensorDataset(valX, valY), batch_size=batchsize)
-    testloader = utils.DataLoader(
-        utils.TensorDataset(testX, testY), batch_size=batchsize)
-
-    # check for gpu.
-    device = "cpu"
-    print(device)
-    # create the model.
-
+    # set up the model
     class Net(nn.Module):
         def __init__(self, in_size, out_size):
             super(Net, self).__init__()
@@ -80,61 +49,22 @@ def main():
             x = self.f(x)
             return x
 
-    model = Net(3, 4).to(device)
+    # initiate the model
+    model = Net(3, 4)
 
+    # set up hyperparameters
     loss_function = nn.BCEWithLogitsLoss()
     optimizer = optim.Adam(model.parameters())
 
-    for epoch in range(1, numEpochs):
-
-        train_loss, valid_loss = [], []
-        # training part
-        model.train()
-        for data, target in trainloader:
-            data, target = data.to(device), target.to(device)
-            optimizer.zero_grad()
-            output = model(data)
-            # print(output)
-            loss = loss_function(output, target)
-            loss.backward()
-            optimizer.step()
-            train_loss.append(loss.item())
-
-        # evaluation part
-        model.eval()
-
-        for data, target in validloader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            loss = loss_function(output, target)
-            valid_loss.append(loss.item())
-
-        print("Epoch:", epoch, "\tTraining Loss: ", round(np.mean(
-            train_loss), 4), "\tValid Loss: ", round(np.mean(valid_loss), 4))
-
-    # test greatness of model
-    model.eval()
-    test_loss = 0
-    num_right = 0
-    with torch.no_grad():
-        for data, target in testloader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            # sum up batch loss
-            for i in range(len(output)):
-                left, right = torch.argmax(output[i]), torch.argmax(target[i])
-                num_right += torch.eq(left, right).item()
-            test_loss += loss_function(output, target).item()
-
-    test_loss /= len(testloader.dataset)
-    accuracy = num_right/ len(testloader.dataset)
-    print('\nTest set: Average loss: {:.4f} \tAccuracy: {:.4f}'.format(
-        test_loss, accuracy))
+    # train the model
+    helpers.trainModel(model, optimizer, loss_function, numEpochs, trainloader, validloader)
+    # test the model
+    helpers.testModel(model, loss_function, numEpochs, testloader, checkAccuracy=True)
 
     #######################################################################
     #                       ** END OF YOUR CODE **
     #######################################################################
-    # illustrate_results_ROI(network, prep)
+    illustrate_results_ROI(model)
 
 
 if __name__ == "__main__":
