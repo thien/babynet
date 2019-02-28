bfa420966176aef3eb3a295084604b71c2667bb5
Alexandre Chenu <mobby@dyn3179-186.wlan.ic.ac.uk>
2019-02-28 17:18:38 +0000

    optimisation added to learn_FM, optimized model added

--- hyper_search.py
+++ hyper_search.py
@@ -12,20 +12,21 @@ from nn_lib import Preprocessor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from bayes_opt import BayesianOptimization
 
+import copy 
 
 seed = 1337
 np.random.seed(seed)
 torch.manual_seed(seed)
 
 defaults = {
-    'l1':20, 
-    'l2':15, 
-    'l3':10, 
-    'l4':7,
-    'lr':1e-3,
+    'l1':300, 
+    'l2':300, 
+    'l3':300, 
+    'l4':300,
+    'lr':0.0008,
     'save':False,
-    'epochs':20,
-    'filepath': './Model/fm_model.pt'
+    'epochs':50,
+    'filepath': 'best_model.pt'
 }
 
 class Net(nn.Module):
@@ -76,10 +77,10 @@ def blackbox(l1,l2,l3,l4,lr):
     # initiate the model
     model = Net(xDim, yDim, l1, l2, l3, l4)
     # set up hyperparameters
-    loss_function = nn.BCEWithLogitsLoss()
+    loss_function = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
     # train the model
-    return -helpers.trainModel(
+    return - helpers.trainModel(
         model, optimizer, loss_function, 
         numEpochs, trainloader, validloader, 
         verbose=False)
@@ -153,7 +154,7 @@ def run(verbose=True, showBot=False, args=defaults):
         xDim, yDim, args['l1'], args['l2'], args['l3'], args['l4'])
 
     # set up hyperparameters
-    loss_function = nn.BCEWithLogitsLoss()
+    loss_function = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=args['lr'])
 
     # train the model
@@ -168,15 +169,105 @@ def run(verbose=True, showBot=False, args=defaults):
     #######################################################################
     #                       ** END OF YOUR CODE **
     #######################################################################
-    if showBot:
-        illustrate_results_ROI(model)
+    #if showBot:
+        #illustrate_results_ROI(model)
 
     return val_loss
+
+def main(args = defaults):
+    dataset = np.loadtxt("FM_dataset.dat")
+    #######################################################################
+    #                       ** START OF YOUR CODE **
+    #######################################################################
+    np.random.shuffle(dataset)
+    
+    trainRatio = 0.8
+    testRatio  = 0.1
+    numEpochs  = 50
+    batchsize  = 64
+
+    # shuffle the dataset prior
+    np.random.shuffle(dataset)
+
+    # preprocess data
+    prep = Preprocessor(dataset)
+    dataset = prep.apply(dataset)
+
+    # retrieve X and Y columns
+    X, Y = dataset[:, 0:3], dataset[:, 3:6]
+
+    # create loaders
+    trainloader, validloader, testloader = helpers.loadTrainingData(
+        X, Y, trainRatio, testRatio, batchsize)
+
+    # initiate the model
+    model = Net(3,3,args['l1'], args['l2'], args['l3'], args['l4'])
+
+    # load hyperparameters
+    loss_function = nn.MSELoss()
+    optimizer     = optim.Adam(model.parameters(), lr = args['lr'])
+
+    # train the model
+    helpers.trainModel(model, optimizer, loss_function, numEpochs, trainloader, validloader)
+    # evaluate the model
+    helpers.testModel(model, loss_function, numEpochs, testloader)
+    
+    torch.save(model.state_dict(), 'best_model_reg.pth')
+    
+    x_store, y_store = [],[]
+    i =0
+    for x,y in testloader:
+        if i < 1: 
+            x_store = np.array(x)
+            y_store = np.array(y)
+        else:
+            np.concatenate((x_store, np.array(x)), axis = 0)
+            np.concatenate((y_store, np.array(y)), axis = 0)
+        i = 1
+
+    evaluate_architecture(model, torch.Tensor(x_store), torch.Tensor(y_store), prep)
+    #######################################################################
+    #                       ** END OF YOUR CODE **
+    #######################################################################
+    # data is normalised in this function
+    #illustrate_results_FM(model, prep)
+
+
+
+def evaluate_architecture(model, X, y, prep):
+    
+    results = model(X)
+    results = results.detach().numpy()
+    dataset = np.concatenate((X.detach().numpy(), y.detach().numpy()), axis = 1)
+    dataset_pred = np.zeros(dataset.shape)
+    dataset_pred[:, 3:6] = results
+    dataset_pred[:, :3] = dataset[:, :3]
+    dataset = prep.revert(dataset)
+    dataset_pred = prep.revert(dataset_pred)
+    prediction = dataset_pred[:, 3:6]
+    true = dataset[:, 3:6]
+    
+    mae = mean_absolute_error(true, prediction)
+    mse = mean_squared_error(true, prediction)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(true, prediction, multioutput = 'uniform_average')
+    accuracy = np.average(1 - np.sqrt((prediction[:,0]-true[:,0])**2+(prediction[:,1]-true[:,1])**2+(prediction[:,2]-true[:,2])**2)/1732)
+    
+    print("\nTest set: Mean absolute error: {:.3f},  R2 score: {:.3f}".format(mae, r2))
+    print("\t  Root mean squared error: {:.3f}\n".format(rmse))
+    print("\t  Average Accuracy: {:.6f}\n".format(accuracy))
+    print("\t  Note: the accuracy is measured as the euclidian distance") 
+    print("\t        between true and prediction over a maximum euclidian error")
+    print("\t        of 1732 (1000 in all 3 directions)")    
+    
+    return mae, mse, rmse, r2
 
 
 if __name__ == "__main__":
 
-	optimise()
+	#optimise()
+	#run()
+	main()
 
 
 ##### BEST RESULT OBTAINED FOR :
--- learn_FM.py
+++ learn_FM.py
@@ -9,15 +9,26 @@ from nn_lib import Preprocessor
 from illustrate import illustrate_results_FM
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
+defaults = {
+    'l1':128, 
+    'l2':64, 
+    'l3':32, 
+    'l4':8,
+    'lr':0.001,
+    'save':False,
+    'epochs':50,
+    'filepath': 'best_model_reg.pth'
+}
+
 # create the model.
 class Net(nn.Module):
-    def __init__(self, in_size, out_size):
+    def __init__(self, in_size, out_size, hidd1, hidd2, hidd3, hidd4):
         super(Net, self).__init__()
-        self.a = nn.Linear(in_size,128)
-        self.d = nn.Linear(128,64)
-        self.e = nn.Linear(64,32)
-        self.g = nn.Linear(32,8)
-        self.f = nn.Linear(8, out_size)
+        self.a = nn.Linear(in_size,hidd1)
+        self.d = nn.Linear(hidd1,hidd2)
+        self.e = nn.Linear(hidd2,hidd3)
+        self.g = nn.Linear(hidd3,hidd4)
+        self.f = nn.Linear(hidd4, out_size)
 
         for m in self.modules():
             if isinstance(m, nn.Linear):
@@ -33,8 +44,7 @@ class Net(nn.Module):
         x = self.f(x)
         return x
     
-    
-def main():
+def main(args = defaults):
     dataset = np.loadtxt("FM_dataset.dat")
     #######################################################################
     #                       ** START OF YOUR CODE **
@@ -61,7 +71,7 @@ def main():
         X, Y, trainRatio, testRatio, batchsize)
 
     # initiate the model
-    model = Net(3,3)
+    model = Net(3,3,args['l1'], args['l2'], args['l3'], args['l4'])
 
     # load hyperparameters
     loss_function = nn.MSELoss()
@@ -92,6 +102,79 @@ def main():
     # data is normalised in this function
     illustrate_results_FM(model, prep)
 
+def blackbox(l1,l2,l3,l4,lr):
+    """
+    Compressed runthrough of training the model.
+    This is used in the BayesianOptimisation function
+    in order to deduce the best hyperparameters during
+    its search. You shouldn't use this to call the
+    model. Use run() instead.
+    """
+    # silly optimiser will make floats of these.
+    l1,l2,l3,l4 = int(l1),int(l2),int(l3),int(l4)
+    # load dataset file
+    dataset = np.loadtxt("FM_dataset.dat")
+    np.random.shuffle(dataset)
+    # setup base parameters
+    trainRatio = 0.8
+    testRatio = 0.1
+    numEpochs = 30
+    batchsize = 64
+    X, Y = dataset[:, 0:3], dataset[:, 3:]
+    xDim, yDim = X.shape[-1], Y.shape[-1]
+    trainloader, validloader, _ = helpers.loadTrainingData(
+        X, Y, trainRatio, testRatio, batchsize)
+    # initiate the model
+    model = Net(xDim, yDim, l1, l2, l3, l4)
+    # set up hyperparameters
+    loss_function = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    # train the model
+    return - helpers.trainModel(
+        model, optimizer, loss_function, 
+        numEpochs, trainloader, validloader, 
+        verbose=False)
+
+def optimise():
+    print("Initiating Bayesian Optimisation...")
+    # initiate parameters
+    params = {
+        'l1': (3, 300), 
+        'l2': (3, 300), 
+        'l3': (3, 300),
+        'l4': (3, 300),
+        'lr': (0.0001, 0.001)
+    }
+
+    # initiate optimiser
+    bayes = BayesianOptimization(
+                f=blackbox,
+                pbounds=params,
+                random_state=seed)
+
+    # search hyperparameter space
+    bayes.maximize(init_points=4,n_iter=100)
+    print("Finished bayesian optimisation.")
+    # retrieve the best hyperparameter
+    best = bayes.max
+
+    # convert layers variables to ints
+    toIntVariableNames = ['l1', 'l2', 'l3', 'l4']
+    for layer in toIntVariableNames:
+        best['params'][layer] = int(best['params'][layer])
+
+    # initiate parameters
+    arguments = copy.deepcopy(defaults)
+    for key in best:
+        arguments[key] = best[key]
+    arguments['save'] = True
+    arguments['epochs'] = 50
+
+    print("Training Model with found parameters.. ", end="")
+    # train the bot with the optimal parameters
+    # and save it.
+    run(verbose=False, showBot=False, args=arguments)
+    print("done.")
 
 
 def evaluate_architecture(model, X, y, prep):
@@ -148,4 +231,5 @@ def predict_hidden(dataset):
 
 
 if __name__ == "__main__":
-    main()
+    #main()
+    predict_hidden("FM_dataset.dat")
